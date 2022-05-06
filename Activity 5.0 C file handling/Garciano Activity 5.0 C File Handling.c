e95999f824c6b1525e6012a77d44e63a091edf56
Steven Kun <89185766+StevenGarciano@users.noreply.github.com>
2022-05-06 17:32:21 +0800

    Update Garciano Activity 5.0 C File Handling.c

--- Activity 5.0 C file handling/Garciano Activity 5.0 C File Handling.c
+++ Activity 5.0 C file handling/Garciano Activity 5.0 C File Handling.c
@@ -7,7 +7,7 @@ FILE * fptr;
 char str [100];
 int n;
 int i=0;
-fptr = fopen("C:\ text.txt","a");
+fptr = fopen("C:\text.txt","a");
 
     printf("Input how many lines you want to be appended: ");
     scanf("%d",&n);
